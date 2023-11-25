87eeb94b9b0fe10435bf5a310e9ec55864144cc1
Krystsina Kalenik <kalenik.kr@gmail.com>
2023-11-25 11:23:46 +0200

    WPF XAML introduction

---
+++ WPF_FirsApp/Phone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_FirsApp
+{
+    internal class Phone
+    {
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Price { get; set; }
+    }
+}
