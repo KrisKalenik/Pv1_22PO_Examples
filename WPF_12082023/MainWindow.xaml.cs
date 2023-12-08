488637fba092f50a61201849dd70e1af22c70834
Krystsina Kalenik <kalenik.kr@gmail.com>
2023-12-09 01:20:26 +0800

    WPF Localisation & Custom Control & Resouce Dictionary

---
+++ WPF_12082023/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_12082023
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Mode_Checked(object sender, RoutedEventArgs e)
+        {
+            var radioButton = sender as RadioButton;
+
+            if(radioButton != null)
+            {
+                string theme = radioButton.Content.ToString().Contains("Light") ? "Light" : "Dark";
+                ChangeTheme(theme);
+            }
+        }
+
+        private void ChangeTheme(string themeName)
+        {
+            var uri = new Uri($"{themeName}Mode.xaml", UriKind.Relative);
+            ResourceDictionary resourceDictionary = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+        }
+    }
+}
